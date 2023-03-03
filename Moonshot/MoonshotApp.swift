76ff965a78bc5ee1eba2803215adb45d3c090ed7
Aymeric <aymeric.pilaert@hotmail.fr>
2023-03-03 08:56:06 +0100

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Aymeric Pilaert on 03/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
