0ebe934af3c29a09d85c95c18e9d6e9a061f2ad6
Aymeric <aymeric.pilaert@hotmail.fr>
2023-03-03 10:52:55 +0100

    added displayName and image to struct missions with computed properties

--- Moonshot/Mission.swift
+++ Moonshot/Mission.swift
@@ -18,4 +18,14 @@ struct Missions: Codable, Identifiable {
     let launchDate: String?
     let crew: [CrewRole]
     let description: String
+    
+    var displayName: String {
+        "Apollo \(id)"
+    }
+    
+    var image: String {
+        "apollo\(id)"
+    }
+    
+    
 }
